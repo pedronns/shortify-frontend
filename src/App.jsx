3e531640b1b25815abd133ceaa47387e2efb4480
Pedro Nunes <pedro.dnunes1@gmail.com>
2025-12-15 12:44:25 -0300

    Feat: Add footer; Style: Add simple gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateLink from "./pages/CreateLink";
 import OpenLink from "./pages/OpenLink";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CreateLink />} />
+      <div className="d-flex flex-column min-vh-100">
+        <main className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<CreateLink />} />
+            <Route path="/:code" element={<OpenLink />} />
+          </Routes>
+        </main>
 
-        <Route path="/:code" element={<OpenLink />} />
-      </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
